f8d3bd26d0bb5af151da053a674e8f1a56de75e6
Adam Alyyan <a64adam@gmail.com>
2013-11-13 13:08:39 -0600

    added script to run status scripts

--- cgi_email.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python /home/ndnmonitor/ndn-status/parse.py
-python /home/ndnmonitor/ndn-status/process.py
-python /home/ndnmonitor/ndn-status/notify.py
-
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python parse.py
+python process.py
